2b085b85082d4b14281dbbcec17688c1997738af
i-i-ijul <haqqikamvret@gmail.com>
2025-05-05 15:32:26 +0700

    menambahkan access

--- fri&function.cpp
+++ fri&function.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class Dosen{
+    public:
+    string nama;
+    void tampilNama(){
+        cout << "namanya adalah " << nama << endl;
+    }
+}; 
+class Staf{
+    public:
+    int nidn;
+};
 int main(){
+    Dosen ds;
+    ds.nama = "giga";
+    ds.tampilNama();
+
+    Dosen *pds = &ds;
+    pds->nama = "reza";
+    cout << "alamat memori = " << pds << endl;
+    pds->tampilNama();
     
+    int a = 5;
+    int b = 3;
+    int *c = &a;
+    *c = 9;
+    cout << endl;
+    cout << a << endl;
+
+    cout << "alamat memori a = " << &a << endl;
+    cout << "alamat memori c = " << c << endl;
+    cout << "cetak c = " << *c << endl;
+    c = &b;
+    cout << "alamat memori c = " << c << endl;
+    cout << "cetak c = " << *c << endl;
 }
